a2bf3165192da6229ec6a9d26a21082cb0759508
alex-charos <realtowz@yahoo.com>
2023-10-20 15:04:35 +0300

    Application class rename

--- bet-service/src/main/java/gr/opap/bet/PlaceBetApplication.java
+++ bet-service/src/main/java/gr/opap/bet/PlaceBetApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BetApplication {
+public class PlaceBetApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BetApplication.class, args);
+        SpringApplication.run(PlaceBetApplication.class, args);
     }
 
 }
