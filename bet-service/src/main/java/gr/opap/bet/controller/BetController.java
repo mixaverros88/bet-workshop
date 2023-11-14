aa662fc1f43bbdf84af3fc67621a50bb27472f20
mixaverros88 <mixalisverros@hotmail.gr>
2023-11-14 09:32:31 +0200

    add swagger configuration

--- bet-service/src/main/java/gr/opap/bet/controller/BetController.java
+++ bet-service/src/main/java/gr/opap/bet/controller/BetController.java
@@ -2,6 +2,7 @@ package gr.opap.bet.controller;
 
 import gr.opap.bet.dto.BetDto;
 import gr.opap.bet.service.BetService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +20,7 @@ public class BetController {
     @Autowired
     BetService betService;
 
+    @Operation(summary = "Add A Bet")
     @PostMapping
     public ResponseEntity place(@Valid @RequestBody final BetDto betDto) {
         betService.place(betDto);
@@ -26,6 +28,7 @@ public class BetController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Get All the Bets")
     @GetMapping
     public List<BetDto> retrieve() {
         return betService.retrieve();
